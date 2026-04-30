5e24627009d504116ed0e75ec079c66b61c7da3c
AryanRogye <aryan.rogye@gmail.com>
2026-04-30 13:32:23 -0500

    rename

--- mlx-finetune/train_model.sh
+++ mlx-finetune/train_model.sh
@@ -9,6 +9,35 @@ if [[ -z "$1" || -z "$2" ]]; then
   exit 1
 fi
 
+# Begin Training
+
 MODEL_NAME="$1"
 FINE_TUNE_FOLDER="$2"
 
+echo "Training $MODEL_NAME"
+
+source ./venv/bin/activate
+
+mlx_lm lora \
+  --model "$MODEL_NAME" \
+  --train \
+  --data "$FINE_TUNE_FOLDER" \
+  --iters 500 \
+  --batch-size 1 \
+  --learning-rate 1e-5 \
+  --max-seq-length 1024 \
+  --grad-accumulation-steps 4 \
+  --save-every 50
+
+echo "Done Training"
+
+echo "Fusing Trained Model"
+
+rm -rf ./output_model
+
+mlx_lm fuse \
+  --model "$MODEL_NAME" \
+  --adapter-path ./adapters \
+  --save-path ./output_model
+
+deactivate
